817f8d4fd6039c1563b3413d77ec7d00b5d84d23
Nikhil Chodavarapu <nikhilchodavarapu@Nikhils-MacBook-Pro.local>
2026-03-05 17:58:33 +0530

    Save posts that are added by user

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/deno";
 import { handleRegister } from "./handlers/register-handler.js";
+import { handlePost } from "./handlers/post-handler.js";
+import { setCookie } from "hono/cookie";
+import { logger } from "hono/logger";
 
 export const createApp = () => {
   const app = new Hono();
+
+  app.use(logger());
+  app.post("/addPost", handlePost);
 
   app.post("/validate", async (c) => {
     const request = await c.req.formData();
@@ -13,8 +19,9 @@ export const createApp = () => {
     const isValidUser = users.some(({ username: name, "new-password": pass }) =>
       name === username && password === pass
     );
-    if (isValidUser) return c.text("HELLO");
-    return c.text("INVALID");
+    if (!isValidUser) return c.redirect("/", 303);
+    setCookie(c, "username", username);
+    return c.redirect("/something.html", 303);
   });
   app.post("/register", handleRegister);
   app.get("*", serveStatic({ root: "./public" }));
--- src/handlers/post-handler.js
+++ src/handlers/post-handler.js
@@ -1,47 +1,32 @@
+import { getCookie } from "hono/cookie";
+
 export const saveImage = async (path, imageData) => {
   const file = await Deno.open(path, { create: true, write: true });
 
   await imageData.stream().pipeTo(file.writable);
 };
 
-export const savePost = (filePath, postInfo) =>
-  fetch(filePath)
-    .then((x) => x.json())
-    .then(async (data) => {
-      data.push(postInfo);
-      await Deno.writeTextFile(JSON.stringify(data, "", 2));
-    });
+export const savePost = (filePath, postInfo) => {
+  const posts = JSON.parse(Deno.readTextFileSync(filePath));
+  posts.push(postInfo);
+  Deno.writeTextFileSync(filePath, JSON.stringify(posts));
+};
 
 export const handlePost = async (c) => {
   const req = await c.req.formData();
-  const { userID } = c.cookie;
-  const { content, imageData } = Object.fromEntries(req.entries());
-  const imgData = JSON.parse(imageData);
+  // console.log(req);
+  const username = getCookie(c).username;
+  const { comment, "post-image": imageData } = Object.fromEntries(
+    req.entries(),
+  );
 
-  const image_name = `${userName}-${imgData.name}`;
-  const imgPath = `/data/post-images/${image_name}`;
+  const image_name = `${username}-${imageData.name}`;
+  const imgPath = `./data/posts/${image_name}`;
 
-  const postInfo = { userID, content, imgPath };
-  const postsPath = `/data/posts/all.posts.json`;
+  const postInfo = { username, comment, imgPath };
+  const postsPath = `./data/all-posts.json`;
 
-  await saveImage(imgPath, imgData);
+  await saveImage(imgPath, imageData);
   await savePost(postsPath, postInfo);
+  return c.text("hello");
 };
-
-const imageData = {
-  name: "newClick.jpg",
-  type: "image/jpg",
-};
-
-const form = new FormData();
-form.append("content", "hello");
-form.append("imageData", JSON.stringify(imageData));
-
-const userName = "nameisnani";
-
-const myC = {
-  req: { formData: async () => await form },
-  cookie: { userID: userName },
-};
-
-handlePost(myC);
