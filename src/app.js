e35eda10427acae45f401d009aa6cec2b8af1b5f
Nikhil Chodavarapu <nikhilchodavarapu@Nikhils-MacBook-Pro.local>
2026-03-05 10:35:34 +0530

    Implements login and register logic

--- src/app.js
+++ src/app.js
@@ -1,45 +1,22 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/deno";
-
-const writeToFiles = async ({ username, "profile-image": fileObject }) => {
-  console.log(fileObject);
-  const file = await Deno.open(
-    `./data/profile-pictures/${username}%${fileObject.name}`,
-    {
-      write: true,
-      create: true,
-    },
-  );
-  await fileObject.stream().pipeTo(file.writable);
-};
-
-const saveUserDetails = async (details) => {
-  const users = JSON.parse(Deno.readTextFileSync("./data/users.json"));
-  users.push({
-    ...details,
-    "profile-image": `${details.username}%${details["profile-image"].name}`,
-  });
-  const file = await Deno.open("./data/users.json", {
-    write: true,
-  });
-  await file.write(new TextEncoder().encode(JSON.stringify(users)));
-  await writeToFiles(details);
-};
+import { handleRegister } from "./handlers/register-handler.js";
 
 export const createApp = () => {
   const app = new Hono();
 
-  app.post("/register", async (c) => {
+  app.post("/validate", async (c) => {
     const request = await c.req.formData();
-    const userInfo = Object.fromEntries(request.entries());
-    saveUserDetails(userInfo);
-    const response = "<h1>hello</h1>";
-
-    const headers = new Headers();
-    headers.append("content-type", "text/html");
-
-    return new Response(response, { status: 200, headers });
+    console.log(request);
+    const { username, password } = Object.fromEntries(request.entries());
+    const users = JSON.parse(Deno.readTextFileSync("./data/users.json"));
+    const isValidUser = users.some(({ username: name, "new-password": pass }) =>
+      name === username && password === pass
+    );
+    if (isValidUser) return c.text("HELLO");
+    return c.text("INVALID");
   });
+  app.post("/register", handleRegister);
   app.get("*", serveStatic({ root: "./public" }));
   return app;
 };
---
+++ src/handlers/register-handler.js
@@ -0,0 +1,36 @@
+const writeToFiles = async ({ username, "profile-image": fileObject }) => {
+  console.log(fileObject);
+  const file = await Deno.open(
+    `./data/profile-pictures/${username}%${fileObject.name}`,
+    {
+      write: true,
+      create: true,
+    },
+  );
+  await fileObject.stream().pipeTo(file.writable);
+};
+
+const saveUserDetails = async (details) => {
+  const users = JSON.parse(Deno.readTextFileSync("./data/users.json"));
+  users.push({
+    ...details,
+    "profile-image": `${details.username}%${details["profile-image"].name}`,
+  });
+  const file = await Deno.open("./data/users.json", {
+    write: true,
+  });
+  await file.write(new TextEncoder().encode(JSON.stringify(users)));
+  await writeToFiles(details);
+};
+
+export const handleRegister = async (c) => {
+  const request = await c.req.formData();
+  const userInfo = Object.fromEntries(request.entries());
+  saveUserDetails(userInfo);
+  const response = "<h1>hello</h1>";
+
+  const headers = new Headers();
+  headers.append("content-type", "text/html");
+
+  return new Response(response, { status: 200, headers });
+};
